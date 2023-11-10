c5a8ccc2ca2789ebc51fa2e631b97ad2b5e393d7
Baur Negmetzhanov <baurneg@gmail.com>
2023-11-10 03:33:11 +0000

    updated exercises

---
+++ 9. conditionals/10.rb
@@ -0,0 +1,8 @@
+#Reformat the following case statement so that it only takes up 5 lines.
+stoplight = ['green', 'yellow', 'red'].sample
+
+case stoplight
+when 'green'then puts 'Go!'
+when 'yellow' then puts 'Slow down!'
+when 'red' then puts 'Stop!'
+end
---
+++ 9. conditionals/6.rb
@@ -0,0 +1,14 @@
+stoplight = ['green', 'yellow', 'red'].sample
+
+#Write a case statement that prints "Go!" if stoplight equals 'green', "Slow down!" 
+#if stoplight equals 'yellow', and "Stop!" if stoplight equals 'red'.
+
+case stoplight
+  when 'green' 
+    print "Go!"
+  when 'yellow' 
+    print "Slow down!"
+  else 'red'
+    print "Stop!!!"
+end
+  
